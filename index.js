13176ace846cf0ae888bf3788512f75e1051ab65
Luke Madel <luke.madel@gmail.com>
2013-10-01 15:15:33 +0200

    first stable version

--- index.js
+++ index.js
@@ -3,13 +3,35 @@
 var path = require('path')
 , basename = path.basename(path.dirname(__filename))
 , debug = require('debug')('mill:contrib:' + basename)
+, util = require('util')
 , Transform = require("stream").Transform
 , CSV = require('csv-string')
+, SqlString = require('./SqlString.js')
 ;
 
-function quotes(str) {
-  return str;
+function escape(str) {
+  return SqlString.escape(str, true);
 }
+function escapeID(str) {
+  return SqlString.escapeId(str);
+}
+
+function Sniffer()
+{
+  this.begin = true
+}
+util.inherits(Sniffer, Transform);
+
+Sniffer.prototype._transform = function (chunk, encoding, done) {
+  if (this.begin) {
+    this.begin = false;
+    this.separator = CSV.detect(chunk);
+  }
+  this.push(chunk, encoding);
+  done()
+}
+
+
 
 function Command(options)
 {
@@ -21,71 +43,88 @@ function Command(options)
   this.titles = [];
   this.counter = 0;
   this.tablename = options.tablename || '{{tablename}}';
-  this.title = options.title;
+  this.createtable = !options.createtable ? false : true;
+  this.title = !options.title ? false : true;
   this.method = (options.method || 'INSERT').toUpperCase();
   this.numrows = options.numrows || 1;
-  this.delimiter = options.delimiter === null ? "\n" : options.delimiter;
-  this.rows = 0;
+  this.delimiter = !options.delimiter ? ";\n" : options.delimiter;
+  this.countrows = 0;
+  this.query = '';
+  this.buffer = '';
+  
 }
 
 Command.prototype = Object.create(
   Transform.prototype, { constructor: { value: Command }});
 
+Command.prototype.parse = function (rows) {
+  var self = this;
+  rows.forEach(function (row) {
+      if (self.counter === 0 && self.title) {
+        self.titles = row.slice(0).map(escapeID);
+        if (self.createtable) {
+          self.query = 'CREATE TABLE IF NOT EXISTS ' + self.tablename + '(';
+          self.query += self.titles.map(function (item) {
+              return item + ' varchar(255)';
+            }
+          ).join(',');
+          self.query += ') ENGINE=MyISAM DEFAULT CHARSET=utf8'
+          self.query = self.query.concat(self.delimiter);
+          self.push(self.query);
+          self.query = '';
+        }
+      }
+      else {
+        self.countrows++;
+        if (self.countrows == 1) {
+          self.query = self.query.concat(self.method, ' INTO ', self.tablename, ' ');
+          if (self.title) {
+            self.query = self.query.concat('(', self.titles.join(','), ') ');
+          }
+          self.query = self.query.concat('VALUES ');
+        }
+        else {
+          self.query = self.query.concat(',');
+        }
+        self.query = self.query.concat('(', row.map(escape).join(','), ')');
+        if (self.countrows >= self.numrows) {
+          self.query = self.query.concat(self.delimiter);
+          self.push(self.query);
+          self.query = '';
+          self.countrows = 0;
+        }
+      }
+      self.counter++;
+    }
+  );
+}
 Command.prototype._transform = function (chunk, encoding, done) {
   var self = this;
 
-  self.buffer += chunk;
-
   if (self.begin) {
     self.begin = false;
-    self.separator = CSV.detect(self.buffer);
+    self.separator = CSV.detect(chunk.toString());
     self.emit('begin');
-  }
 
-  var r, s = 0, ret = '';
-
-  while (r = CSV.read(self.buffer.slice(s), this.separator, function (row) {
-        if (row[0] === '' && row.length === 1) {
-          return;
-        }
-        if (self.counter === 0 && self.title) {
-          self.titles = row.slice(0).map(quotes);
-        }
-        else {
-          self.rows++;
-          if (self.rows === 1) {
-            ret += self.method + ' INTO ' + self.tablename + ' ';
-            if (self.title) {
-              ret += '(' + self.titles.join(',') + ') ';
-            }
-            ret += 'VALUES ';
-          }
-          else {
-            ret += ',';
-          }
-          ret += '(' + row.map(quotes).join(',') + ')';
-          if (self.rows >= self.numrows) {
-            ret += ';' + self.delimiter;
-            self.rows = 0;
-          }
-        }
-        self.counter++;
-      }
-    )
-  ) {
-    s += r;
   }
-  self.push(ret);
-  self.buffer = self.buffer.slice(s);
-}
-Command.prototype._flush = function (done) {
-  var self = this;
-  self.push(';');
-  self.rows = 0;
+  self.buffer = self.buffer.concat(chunk.toString());
+  var x = CSV.readChunk(self.buffer, self.separator, function (rows) {
+      self.parse(rows);
+    }
+  );
   done();
-};
+  self.buffer = self.buffer.slice(x);
+}
 Command.prototype.end = function () {
   var self = this;
+  CSV.readAll(self.buffer, self.separator, function (rows) {
+      self.parse(rows);
+    }
+  );
+  if (self.countrows !== 0) {
+    self.query = self.query.concat(self.delimiter);
+    self.push(self.query);
+  }
   self.emit('end');
 };
 
--- test.js
+++ test.js
@@ -11,11 +11,11 @@ var  path = require('path')
 describe(basename, function () {
 
     describe('#1', function () {
-        it('should return json', function (done) {
+        it('should return sql', function (done) {
             tester(command, {tablename: 'test', title: true, delimiter: '/'})
             .send('xxx,yyy,zzz\nA,B,C\nD,E,F\n')
             .end(function (err, res) {
-                res.should.equal('INSERT INTO test (xxx,yyy,zzz) VALUES (A,B,C);/INSERT INTO test (xxx,yyy,zzz) VALUES (D,E,F);/')
+                res.should.equal("INSERT INTO test (`xxx`,`yyy`,`zzz`) VALUES ('A','B','C')/INSERT INTO test (`xxx`,`yyy`,`zzz`) VALUES ('D','E','F')/")
                 done();
               }
             );
@@ -24,11 +24,11 @@ describe(basename, function () {
       }
     )
     describe('#2', function () {
-        it('should return json', function (done) {
+        it('should return sql', function (done) {
             tester(command, {tablename: 'test', title: false})
             .send('xxx,yyy,zzz\nA,B,C\nD,E,F\n')
             .end(function (err, res) {
-                res.should.equal('INSERT INTO test VALUES (xxx,yyy,zzz);\nINSERT INTO test VALUES (A,B,C);\nINSERT INTO test VALUES (D,E,F);\n')
+                res.should.equal("INSERT INTO test VALUES ('xxx','yyy','zzz');\nINSERT INTO test VALUES ('A','B','C');\nINSERT INTO test VALUES ('D','E','F');\n")
                 done();
               }
             );
@@ -37,11 +37,11 @@ describe(basename, function () {
       }
     )
     describe('#3', function () {
-        it('should return json', function (done) {
+        it('should return sql', function (done) {
             tester(command, {tablename: 'test', title: false, numrows: 2, method: 'replace'})
             .send('xxx,yyy,zzz\nA,B,C\nD,E,F\nG,H,I\n')
             .end(function (err, res) {
-                res.should.equal('REPLACE INTO test VALUES (xxx,yyy,zzz),(A,B,C);\nREPLACE INTO test VALUES (D,E,F),(G,H,I);\n')
+                res.should.equal("REPLACE INTO test VALUES ('xxx','yyy','zzz'),('A','B','C');\nREPLACE INTO test VALUES ('D','E','F'),('G','H','I');\n")
                 done();
               }
             );
@@ -50,11 +50,11 @@ describe(basename, function () {
       }
     )
     describe('#4', function () {
-            it('should return json', function (done) {
+            it('should return sql', function (done) {
             tester(command, {tablename: 'test', title: false, numrows: 3})
             .send('xxx,yyy,zzz\nA,B,C\nD,E,F\nG,H,I\n')
             .end(function (err, res) {
-                res.should.equal('INSERT INTO test VALUES (xxx,yyy,zzz),(A,B,C),(D,E,F);\nINSERT INTO test VALUES (G,H,I)')
+                res.should.equal("INSERT INTO test VALUES ('xxx','yyy','zzz'),('A','B','C'),('D','E','F');\nINSERT INTO test VALUES ('G','H','I');\n")
                 done();
               }
             );
@@ -62,5 +62,47 @@ describe(basename, function () {
         )
       }
     )
+    describe('#5', function () {
+            it('should return sql', function (done) {
+            tester(command, {tablename: 'test', title: true, numrows : 3, createtable: true})
+            .send('xxx,yyy,zzz\nA,B,C\nD,E,F\nG,H,I\n')
+            .end(function (err, res) {
+                res.should.equal("CREATE TABLE IF NOT EXISTS test(`xxx` varchar(255),`yyy` varchar(255),`zzz` varchar(255)) ENGINE=MyISAM DEFAULT CHARSET=utf8;\nINSERT INTO test (`xxx`,`yyy`,`zzz`) VALUES ('A','B','C'),('D','E','F'),('G','H','I');\n")
+                done();
+              }
+            );
+          }
+        )
+      }
+    )
+    describe('#6', function () {
+        it('should return sql', function (done) {
+            tester(command, {tablename: 'test', title: true})
+            .send('xxx,yyy,zzz\nA,"B\nB",C\nD,E,F\n')
+            .end(function (err, res) {
+                res.should.equal("INSERT INTO test (`xxx`,`yyy`,`zzz`) VALUES ('A','B\\nB','C');\nINSERT INTO test (`xxx`,`yyy`,`zzz`) VALUES ('D','E','F');\n")
+                done();
+              }
+            );
+          }
+        )
+      }
+    )
+    describe('#7', function () {
+        it('should return sql', function (done) {
+            tester(command, {tablename: 'test', title: true})
+            .send('xxx,yyy,zzz\nA,"B;B",C\nD,E,F\n')
+            .end(function (err, res) {
+                res.should.equal("INSERT INTO test (`xxx`,`yyy`,`zzz`) VALUES ('A','B;B','C');\nINSERT INTO test (`xxx`,`yyy`,`zzz`) VALUES ('D','E','F');\n")
+                done();
+              }
+            );
+          }
+        )
+      }
+    )
+
+
+
   }
 );
